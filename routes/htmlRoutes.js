5f1ecba991dd3cc3b78c6bd4e55ded4a2f33a589
TheNoodleMoose <cjh605@gmail.com>
2018-12-21 14:28:37 -0500

    Adding initial files and file structure

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,33 @@
+// var db = require("../models");
+
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
+  // Render 404 page for any unmatched routes
+};
