6830964afb4ff2f374b18745ee43d66605b5c949
Richelle Burke <burkeri@alumni.upenn.edu>
2019-01-02 10:49:24 -0500

    cleaned up extra files

--- src/main.js
+++ src/main.js
@@ -9,11 +9,6 @@ import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 Vue.use(BootstrapVue);
 
-// Vue-Visual
-import VueVisual from "vue-visual";
-Vue.component("visual", VueVisual);
-require("vue-visual/index.css");
-
 Vue.config.productionTip = false;
 
 sync(store, router);
