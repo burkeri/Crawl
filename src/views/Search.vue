29171e53c2b8551e24a360d231ef420e50c724df
Richelle Burke <burkeri@alumni.upenn.edu>
2019-01-04 11:07:51 -0500

    changed price buttons to radio

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -23,14 +23,13 @@
     <!-- Price Select -->
     <h3 id="price">Price</h3>
 
-    <b-form-checkbox-group
-      v-bind:class="{active: isActive}"
+    <b-form-radio-group
       buttons
       id="brand"
       size="lg"
       name="ds"
       :options="options"
-    ></b-form-checkbox-group>
+    ></b-form-radio-group>
 
     <!-- Search button -->
     <b-button id="search">Search</b-button>
@@ -45,9 +44,9 @@ export default {
   data() {
     return {
       options: [
-        { text: "$", value: "$" },
-        { text: "$$", value: "$$" },
-        { text: "$$$", value: "$$$" }
+        { text: "$", value: "1" },
+        { text: "$$", value: "2" },
+        { text: "$$$", value: "3" }
       ]
     };
   }
